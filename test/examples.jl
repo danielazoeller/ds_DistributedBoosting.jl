285265c607da6e6587042d32f2f9337a3e4208db
Daniela Zöller <zoeller@imbi.uni-freiburg.de>
2019-01-22 15:24:16 +0100

    Added results of test data to example

--- test/examples.jl
+++ test/examples.jl
@@ -13,8 +13,8 @@ YourServernames = ["Server1","Server2"]
 # Basic parameters
 YourStepno = 30
 YourEndpoint = "Y"
-YourStartBuffer = 20
-YourRunBuffer = 20
+YourStartBuffer = 2
+YourRunBuffer = 2
 YourCheck = true
 YourTolerance=0.00000000000005
 
@@ -27,9 +27,21 @@ YourLabels = ["X1","X2","X3","X4","X5","X6","X7","X8","X9","X10"]
 result = ds_boosting(YourStepno, YourEndpoint, YourStartBuffer, YourRunBuffer,
             YourRpath, YourUrls,YourUser,YourPassword,YourTable,
             YourServernames,YourCheck)
+# Selected variables are: 
+unique(result.selections)
+# For the example data, covariances are called 6 times. 
+# Of a=2 variables before starting, for additional 5 after 1 boosting step,
+# for additional 4 after 7 boosting steps, 4 after 10, 5 after 14 and 3 after 25.
+# 9 unique variables are selected:
+# X24, X9, X21, X30, X27, X18, X6, X12, X15
 
-# Perform Boosting for selected variables (YourLabels)
+# Perform Boosting for selected variables (YourLabels). Check will be performed only for these.
 result_sel = ds_boosting(YourStepno, YourEndpoint, YourStartBuffer, YourRunBuffer,
             YourRpath, YourUrls,YourUser,YourPassword,YourTable,
             YourServernames,YourCheck,YourWarning, YourTolerance,
             YourShrink, YourMaxvar, YourLabels)
+# For the example data, covariances are called 3 times.
+# Of a=2 variables before starting, for additional 4 after 4 boosting steps,
+# and 3 after 6 boosting steps.
+# 6 unique variables are selected:
+# X9, X6, X3, X5, X10, X8
